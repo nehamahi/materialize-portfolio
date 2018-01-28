7d2fbe943736280110e5b74b94b5b37f633f90a4
nehamahi <nehamaheshwari89@gmail.com>
2018-01-28 12:45:28 +0530

    Added app.js and moved static files to public dir

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+
+// Define the port to run on
+app.set('port', 8080);
+
+console.log('__dirname : ', path.join(__dirname, 'public'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/test-api', function (req, res) {
+  res.send(req.params)
+});
+
+// Listen for requests
+var server = app.listen(app.get('port'), function() {
+  var port = server.address().port;
+  console.log('Magic happens on port ' + port);
+});
